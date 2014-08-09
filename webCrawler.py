146649f07efafb479a5d92b41073b5524823fa07
sjoelee <sammy.joe.lee@gmail.com>
2014-08-08 19:56:30 -0700

    first commit with basic tests on BeautifulSoup

---
+++ webCrawler.py
@@ -0,0 +1,24 @@
+import urlparse
+import urllib
+from bs4 import BeautifulSoup
+
+#url = "http://www.yellowpages.com/tucson-az/cupcakes?g=tucson%2C%20az&q=cupcakes"
+
+url = "http://www.yellowpages.com/tucson-az/mip/cupcakes-456735205?lid=456735205"
+
+urls = [url] # stack of urls to scrape
+visited = [url]
+
+while len(urls) > 0:
+    try:
+        htmltext = urllib.urlopen(urls[0]).read()
+    except:
+        print urls[0]
+    soup = BeautifulSoup(htmltext)
+
+    urls.pop(0)
+
+#    print soup.prettify()
+
+    print soup.get_text()
+        
