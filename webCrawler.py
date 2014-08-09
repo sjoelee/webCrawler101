eb5e8cc2ba8fcbfcd78283680283e04f6db6f135
sjoelee <sammy.joe.lee@gmail.com>
2014-08-09 12:20:42 -0700

    modularizing code and recorded output

--- webCrawler.py
+++ webCrawler.py
@@ -2,39 +2,49 @@ import urlparse
 import urllib
 from bs4 import BeautifulSoup
 
-url = "http://www.yellowpages.com/tucson-az/cupcakes?g=tucson%2C%20az&q=cupcakes"
 
 #url = "http://www.yellowpages.com/tucson-az/mip/cupcakes-456735205?lid=456735205"
 
-urls = [url] # stack of urls to scrape
-visited = [url]
-businessNames = []
-pageResults = []
 
-while len(urls) > 0:
-    try:
-        htmltext = urllib.urlopen(urls[0]).read()
-    except:
-        print urls[0]
-    soup = BeautifulSoup(htmltext)
-
-    urls.pop(0)
-
-    # What if soup is null?? Need to handle this exception
-
+def getPageResults(url, soup):
+    pageResults = []
     # Store all results into a list
     pageResults.append(url)
     for tag in soup.findAll('a', attrs={"data-remote":"true"}, href=True):
         pageResults.append(urlparse.urljoin(url, tag['href']))
 
-    print pageResults
-    for result in pageResults:
-        htmltext = urllib.urlopen(result).read()
-        soup = BeautifulSoup(htmltext)
-        for tag in soup.findAll('a', class_="business-name"):
-            if tag.span:
-                businessNames.append(tag.span.renderContents())
-                print tag.span.renderContents()
+    return pageResults
 
-    print "Number of busineses on this page:", len(businessNames)
-        
+def main():
+    url = "http://www.yellowpages.com/tucson-az/cupcakes?g=tucson%2C%20az&q=cupcakes"
+    pageResults = []
+    urls = [url] # stack of urls to scrape
+    visited = [url]
+    businessNames = []
+
+    while len(urls) > 0:
+        try:
+            htmltext = urllib.urlopen(urls[0]).read()
+        except:
+            print urls[0]
+        soup = BeautifulSoup(htmltext)
+    
+        urls.pop(0)
+    
+        # What if soup is null?? Need to handle this exception
+        pageResults = getPageResults(url, soup)
+        print pageResults
+
+        for result in pageResults:
+            htmltext = urllib.urlopen(result).read()
+            soup = BeautifulSoup(htmltext)
+            for tag in soup.findAll('a', class_="business-name"):
+                if tag.span:
+                    businessNames.append(tag.span.renderContents())
+                    print tag.span.renderContents()
+    
+        print "Number of busineses on this page:", len(businessNames)
+            
+if __name__=="__main__":
+    main()
+    
