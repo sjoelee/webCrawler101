f3a074150c375c6e3f8870840709006e495bc083
sjoelee <sammy.joe.lee@gmail.com>
2014-08-12 22:28:59 -0700

    Scrapy implementation of webCrawler

---
+++ yellowPages/yellowPages/settings.py
@@ -0,0 +1,16 @@
+# Scrapy settings for yellowPages project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'yellowPages'
+BOT_VERSION = '1.0'
+
+SPIDER_MODULES = ['yellowPages.spiders']
+NEWSPIDER_MODULE = 'yellowPages.spiders'
+DEFAULT_ITEM_CLASS = 'yellowPages.items.YellowpagesItem'
+USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+
