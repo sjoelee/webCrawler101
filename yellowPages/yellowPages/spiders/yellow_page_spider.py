cf766c36ff87eb12c025ea3096bbb84bd7975b92
sjoelee <sammy.joe.lee@gmail.com>
2014-08-13 11:53:33 -0700

    output results to file

--- yellowPages/yellowPages/spiders/yellow_page_spider.py
+++ yellowPages/yellowPages/spiders/yellow_page_spider.py
@@ -14,6 +14,8 @@ class YellowPageSpider(BaseSpider):
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
         businessIDs = hxs.select('//*[@id="main-content"]/div[4]/div[3]/div/@id').extract()
+
+        businesses = []
         for business in businessIDs:
             businessXPath = '//*[@id="'+business+'"]/div/div[2]/div[2]'
             suffixBusinessName    = '/h3/a[1]/span/text()'
@@ -21,20 +23,16 @@ class YellowPageSpider(BaseSpider):
             suffixBusinessPostal  = '/div/div[1]/p/span[4]/text()'
             suffixBusinessPhone   = '/div/div[1]/ul/li/text()'
 
-            item = YellowpagesItem()
-            item['businessName']   = hxs.select(businessXPath + suffixBusinessName).extract()
-            item['businessStreet'] = hxs.select(businessXPath + suffixBusinessStreet).extract()
-            item['businessPostal'] = hxs.select(businessXPath + suffixBusinessPostal).extract()
-            item['businessPhone']  = hxs.select(businessXPath + suffixBusinessPhone).extract()
-            print item['businessName']
-            print item['businessStreet']
-            print item['businessPostal']
-            print item['businessPhone']
-        # sites = hxs.select('//*[@id="main-content"]/div[4]/div[3]/div/@id').extract()
-        # for site in sites:
-        #     print site.select('./@id').extract()
-            # item = YellowpagesItem()
-            # item['title'] = site.select('text()').extract()
-            # print item['title']
-        # filename = "cupcakes"
-        # open(filename, 'wb').write(response.body)
+            businessItem = YellowpagesItem()
+            businessItem['businessName']   = hxs.select(businessXPath + suffixBusinessName).extract()
+            businessItem['businessStreet'] = hxs.select(businessXPath + suffixBusinessStreet).extract()
+            businessItem['businessPostal'] = hxs.select(businessXPath + suffixBusinessPostal).extract()
+            businessItem['businessPhone']  = hxs.select(businessXPath + suffixBusinessPhone).extract()
+            # print businessItem['businessName']
+            # print businessItem['businessStreet']
+            # print businessItem['businessPostal']
+            # print businessItem['businessPhone']
+
+            businesses.append(businessItem)
+
+        return businesses
