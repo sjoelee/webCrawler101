06b6567071c88a2996c65d7bc3b531bd07f27084
sjoelee <sammy.joe.lee@gmail.com>
2014-08-13 00:28:05 -0700

    Fixed bug for starting URL in spider

--- yellowPages/yellowPages/spiders/yellow_page_spider.py
+++ yellowPages/yellowPages/spiders/yellow_page_spider.py
@@ -3,10 +3,13 @@ from scrapy.selector import HtmlXPathSelector
 
 class YellowPageSpider(BaseSpider):
     name = "yellowpages.com"
-    allowed_domains = [
-        "http://www.yellowpages.com/tucson-az/cupcakes?g=tucson%2C%20az&q=cupcakes"
+    allowed_domains = ["yellowpages.com"]
+    start_urls = [
+        "http://www.yellowpages.com/tucson-az/cupcakes?g=tucson%2C%20az&q=cupcakes",
+#        "http://www.yellowpages.com/tucson-az/mip/cupcakes-456735205?lid=456735205"
         ]
 
     def parse(self, response):
+#        filename = response.url.split("/")[-2]#"cupcakes"
         filename = "cupcakes"
         open(filename, 'wb').write(response.body)
