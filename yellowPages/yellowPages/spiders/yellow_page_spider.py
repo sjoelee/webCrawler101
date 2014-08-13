87fe32f0e4b5ff11af89f39d92a02ddf00bda1ca
sjoelee <sammy.joe.lee@gmail.com>
2014-08-13 06:47:41 -0700

    Added postal codes

--- yellowPages/yellowPages/spiders/yellow_page_spider.py
+++ yellowPages/yellowPages/spiders/yellow_page_spider.py
@@ -16,15 +16,20 @@ class YellowPageSpider(BaseSpider):
         businessIDs = hxs.select('//*[@id="main-content"]/div[4]/div[3]/div/@id').extract()
         for business in businessIDs:
             businessXPath = '//*[@id="'+business+'"]/div/div[2]/div[2]'
-            suffixBusinessName = '/h3/a[1]/span/text()'
-            suffixBusinessStreet = '/div/div[1]/p/span[1]/text()'
-            suffixBusinessZipCode = '/div/div[1]/p/span[4]/text()'
-            businessName = hxs.select(businessXPath + suffixBusinessName).extract()
-            businessStreet = hxs.select(businessXPath + suffixBusinessStreet).extract()
-            businessPostal = hxs.select(businessXPath + suffixBusinessZipCode).extract()
-            print businessName
-            print businessStreet
-            print businessPostal
+            suffixBusinessName    = '/h3/a[1]/span/text()'
+            suffixBusinessStreet  = '/div/div[1]/p/span[1]/text()'
+            suffixBusinessPostal  = '/div/div[1]/p/span[4]/text()'
+            suffixBusinessPhone   = '/div/div[1]/ul/li/text()'
+
+            item = YellowpagesItem()
+            item['businessName']   = hxs.select(businessXPath + suffixBusinessName).extract()
+            item['businessStreet'] = hxs.select(businessXPath + suffixBusinessStreet).extract()
+            item['businessPostal'] = hxs.select(businessXPath + suffixBusinessPostal).extract()
+            item['businessPhone']  = hxs.select(businessXPath + suffixBusinessPhone).extract()
+            print item['businessName']
+            print item['businessStreet']
+            print item['businessPostal']
+            print item['businessPhone']
         # sites = hxs.select('//*[@id="main-content"]/div[4]/div[3]/div/@id').extract()
         # for site in sites:
         #     print site.select('./@id').extract()
