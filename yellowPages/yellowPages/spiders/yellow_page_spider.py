f6393c45a9b5e1eebd8c9b89f8a250a7d83bdcdd
sjoelee <sammy.joe.lee@gmail.com>
2014-08-13 03:53:05 -0700

    Slowly walking through the tree

--- yellowPages/yellowPages/spiders/yellow_page_spider.py
+++ yellowPages/yellowPages/spiders/yellow_page_spider.py
@@ -6,16 +6,18 @@ from yellowPages.items import YellowpagesItem
 class YellowPageSpider(BaseSpider):
     name = "yellowpages.com"
     allowed_domains = ["yellowpages.com"]
+    # start with one page
     start_urls = [
         "http://www.yellowpages.com/tucson-az/cupcakes?g=tucson%2C%20az&q=cupcakes"
         ]
 
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
-        sites = hxs.select('///div')
+        sites = hxs.select('//*[@id="main-content"]/div[4]/div[3]/div')
         for site in sites:
-            item = YellowpagesItem()
-            item['title'] = site.select('text()').extract()
-            print item['title']
+            print site
+            # item = YellowpagesItem()
+            # item['title'] = site.select('text()').extract()
+            # print item['title']
         # filename = "cupcakes"
         # open(filename, 'wb').write(response.body)
