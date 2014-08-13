8130cd27a46bbfb8f318ea3656a3ec7f86e5f27b
sjoelee <sammy.joe.lee@gmail.com>
2014-08-13 01:26:07 -0700

    Playing around with XPath and added notes to keep track of progress

--- yellowPages/yellowPages/spiders/yellow_page_spider.py
+++ yellowPages/yellowPages/spiders/yellow_page_spider.py
@@ -1,15 +1,21 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 
+from yellowPages.items import YellowpagesItem
+
 class YellowPageSpider(BaseSpider):
     name = "yellowpages.com"
     allowed_domains = ["yellowpages.com"]
     start_urls = [
-        "http://www.yellowpages.com/tucson-az/cupcakes?g=tucson%2C%20az&q=cupcakes",
-#        "http://www.yellowpages.com/tucson-az/mip/cupcakes-456735205?lid=456735205"
+        "http://www.yellowpages.com/tucson-az/cupcakes?g=tucson%2C%20az&q=cupcakes"
         ]
 
     def parse(self, response):
-#        filename = response.url.split("/")[-2]#"cupcakes"
-        filename = "cupcakes"
-        open(filename, 'wb').write(response.body)
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('///div')
+        for site in sites:
+            item = YellowpagesItem()
+            item['title'] = site.select('text()').extract()
+            print item['title']
+        # filename = "cupcakes"
+        # open(filename, 'wb').write(response.body)
