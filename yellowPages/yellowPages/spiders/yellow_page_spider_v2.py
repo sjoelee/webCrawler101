c6c44a7d75916d528bde4eb00e1677dd4370c5f9
sjoelee <sammy.joe.lee@gmail.com>
2014-08-15 14:55:54 -0700

    Latest changes with rules

--- yellowPages/yellowPages/spiders/yellow_page_spider_v2.py
+++ yellowPages/yellowPages/spiders/yellow_page_spider_v2.py
@@ -12,11 +12,18 @@ class YellowPageSpider(CrawlSpider):
         "http://www.yellowpages.com/tucson-az/cupcakes?g=tucson%2C%20az&q=cupcakes"
         ]
 
+    rules = (
+        Rule(SgmlLinkExtractor(allow=('//ul/li[\d]/a',),
+                               restrict_xpaths=('//*[@id="main-content"]/div[4]/div[5]/',)),
+                               callback='parse_page', follow=True),
+        )
     def parse(self, response):
+        self.parse_page(response)
+        businesses = []
+
         hxs = HtmlXPathSelector(response)
         businessIDs = hxs.select('//*[@id="main-content"]/div[4]/div[3]/div/@id').extract()
 
-        businesses = []
         for business in businessIDs:
             businessXPath = '//*[@id="'+business+'"]/div/div[2]/div[2]'
             suffixBusinessName    = '/h3/a[1]/span/text()'
@@ -32,10 +39,42 @@ class YellowPageSpider(CrawlSpider):
 
             businesses.append(businessItem)
 
-        nextURL = hxs.select('//*[@id="main-content"]/div[4]/div[5]/ul/li[4]/a/@href').extract()[0]
+        # liTags = hxs.select('//*[@id="main-content"]/div[4]/div[5]/ul').extract()
+        
+        # for li in liTags:
+        #     print type(li)
+        #     print li.select('./a/@href').extract()
+
+        nextURL = hxs.select('//*[@id="main-content"]/div[4]/div[5]/ul/li[2]/a/@href').extract()[0]
+        print "next page is %s " % nextURL
+        nextURL = "www.yellowpages.com"+nextURL
+        print "next page is %s " % nextURL
         if nextURL:
             request = Request(url=nextURL, callback=self.parse_page,)
         return businesses
 
     def parse_page(self, response):
-        self.log("Visisted %s" % response.url)
+        print "Visited 2nd page!"
+
+        businesses = []
+
+        hxs = HtmlXPathSelector(response)
+        businessIDs = hxs.select('//*[@id="main-content"]/div[4]/div[3]/div/@id').extract()
+
+        for business in businessIDs:
+            businessXPath = '//*[@id="'+business+'"]/div/div[2]/div[2]'
+            suffixBusinessName    = '/h3/a[1]/span/text()'
+            suffixBusinessStreet  = '/div/div[1]/p/span[1]/text()'
+            suffixBusinessPostal  = '/div/div[1]/p/span[4]/text()'
+            suffixBusinessPhone   = '/div/div[1]/ul/li/text()'
+
+            businessItem = YellowpagesItem()
+            businessItem['businessName']   = hxs.select(businessXPath + suffixBusinessName).extract()
+            businessItem['businessStreet'] = hxs.select(businessXPath + suffixBusinessStreet).extract()
+            businessItem['businessPostal'] = hxs.select(businessXPath + suffixBusinessPostal).extract()
+            businessItem['businessPhone']  = hxs.select(businessXPath + suffixBusinessPhone).extract()
+
+            businesses.append(businessItem)
+
+        nextURL = hxs.select('//*[@id="main-content"]/div[4]/div[5]/ul/li[2]/a/@href').extract()[0]
+        yield Request(nextURL, callback=self.parse_page,)
